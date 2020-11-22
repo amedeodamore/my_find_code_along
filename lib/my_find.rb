8617d55c35c5c0d4027a72cedb643ba8a9f84e9f
Amedeo d'Amore <amedeodamore@gmail.com>
2020-11-22 15:23:44 +0000

    commit tree 233678eac5c351abfd2c52eec502af39deb6de97

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,9 @@
 require 'pry'
 
 def my_find(collection)
-
-end
+    counter = 0
+    while counter < collection.length
+      return collection[counter] if yield(collection[counter])
+      counter = counter + 1
+    end
+  end 
